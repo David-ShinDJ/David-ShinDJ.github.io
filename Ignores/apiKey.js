da1e980f397e3911d224d3c074229da44bf616b3
DavidShin <dnrur124@gmail.com>
2023-01-03 15:22:49 +0900

    깃이그노어적용

--- Ignores/apiKey.js
+++
@@ -1,7 +0,0 @@
-
-
-
-
-module.exports.OPEN_API_KEY = "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"
-module.exports.NAVER_CLIENT_ID = "eOXk_4ITmPMWybfjduQo" 
-module.exports.NAVER_CLIENT_SECRET = "OLbstU4jyL"
